011dad093af1298d88874c8850f1136956a09029
tahti <piotr@bauzy.uni.lux>
2012-11-17 12:07:05 +0100

    home

--- bin/src/vimfm-0.7.2/src/version.c
+++
@@ -1,1 +0,0 @@
-const char GIT_HASH[] = "";
--- bin/src/vimfm-0.7.2/tests/keys/builtin_keys.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BUILTIN_KEYS_H__
-#define __BUILTIN_KEYS_H__
-
-void init_builtin_keys(int *key_mode);
-
-#endif
-
-/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab : */
