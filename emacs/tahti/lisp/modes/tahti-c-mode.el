198cfe11f76762a57148ac1b7b3aa61c8dd82cc8
Piotr Kordy <pkordy@googlemail.com>
2013-01-19 16:42:27 +0100

    added whitespace mode for lzz files

--- emacs/tahti/lisp/modes/tahti-c-mode.el
+++ emacs/tahti/lisp/modes/tahti-c-mode.el
@@ -1,4 +1,5 @@
 (require 'tahti-keys)
 (setq auto-mode-alist (cons '("\\.lzz$" . c-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.lzz$" . whitespace-mode) auto-mode-alist))
 (provide 'tahti-c-mode)
 
