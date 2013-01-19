ab177f603275bd06a710320787d75306120538e6
Piotr Kordy <pkordy@googlemail.com>
2013-01-19 16:49:50 +0100

    error correction

--- emacs/tahti/lisp/modes/tahti-c-mode.el
+++ emacs/tahti/lisp/modes/tahti-c-mode.el
@@ -1,5 +1,5 @@
 (require 'tahti-keys)
+(add-hook 'c-mode-hook 'whitespace-mode)
 (setq auto-mode-alist (cons '("\\.lzz$" . c-mode) auto-mode-alist))
-(add-hook 'c-mode 'whitespace-mode)
 (provide 'tahti-c-mode)
 
