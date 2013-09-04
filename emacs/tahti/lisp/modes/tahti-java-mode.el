07b071cc8c593e347b0222a07b3ea4987b69e1ab
Piotr Kordy <pkordy@googlemail.com>
2013-09-04 14:15:34 +0200

    Added indenting on enter in emacs

--- emacs/tahti/lisp/modes/tahti-java-mode.el
+++ emacs/tahti/lisp/modes/tahti-java-mode.el
@@ -8,6 +8,7 @@
   ;;Treat Java 1.5 @-style annotations as comments
   (setq c-comment-start-regexp "(@|/(/|[*][*]?))")
   (modify-syntax-entry ?@ "< b" java-mode-syntax-table)
+  (local-set-key (kbd "RET") 'newline-and-indent)) ; indent on enter
 )
 (add-hook 'java-mode-hook 'tahti-java-mode-init)
 (provide 'tahti-java-mode)
