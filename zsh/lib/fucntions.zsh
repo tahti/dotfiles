6b4d7491aa0e0cefec15fc289b716cbd3faece79
Piotr Kordy <pkordy@googlemail.com>
2014-02-13 14:02:10 +0100

    Added some crypto functions to zsh

--- zsh/lib/fucntions.zsh
+++ zsh/lib/fucntions.zsh
@@ -5,3 +5,16 @@ autoload -Uz any
 autoload -Uz myip
 autoload -Uz givedef
 autoload -Uz open
+mcd() { mkdir -p "$1" && cd "$1"; }
+pj() { python -mjson.tool } # pretty-print JSON
+cj() { curl -sS $@ | pj } # curl JSON
+md5() { echo -n $1 | openssl md5 /dev/stdin }
+sha1() { echo -n $1 | openssl sha1 /dev/stdin }
+sha256() { echo -n $1 | openssl dgst -sha256 /dev/stdin }
+sha512() { echo -n $1 | openssl dgst -sha512 /dev/stdin }
+rot13() { echo $1 | tr "A-Za-z" "N-ZA-Mn-za-m" }
+rot47() { echo $1 | tr "\!-~" "P-~\!-O" }
+latrus() { echo $1 | tr "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM" "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ" }
+ruslat() { echo $1 | tr "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ" "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM" }
+urlencode() { python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" $1 }
+urldecode() { python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])" $1 }
