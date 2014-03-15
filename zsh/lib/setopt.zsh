0b508214fd1a76459819f90aa42545167a076d93
Piotr Kordy <pkordy@googlemail.com>
2014-03-15 07:09:59 +0100

    Not sharing history

--- zsh/lib/setopt.zsh
+++ zsh/lib/setopt.zsh
@@ -20,7 +20,7 @@ setopt hist_ignore_space  # remove command line from history list when first cha
 setopt hist_find_no_dups  # When searching history don't display results already cycled through twice
 setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
 setopt hist_verify        # don't execute, just expand history
-setopt share_history      # imports new commands and appends typed commands to history
+#setopt share_history      # imports new commands and appends typed commands to history
 
 # ===== Completion 
 setopt always_to_end  # When completing from the middle of a word, move the cursor to the end of the word    
