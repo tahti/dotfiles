fe0f5d935b900478eee8b2c0c23f3ee06a0004d7
Piotr Kordy <pkordy@googlemail.com>
2016-03-22 08:58:27 +0100

    Markdown

---
+++ emacs/tahti/lisp/modes/tahti-markdown-mode.el
@@ -0,0 +1,15 @@
+(push 'markdown-mode el-get-packages)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(defun tahti-markdown-mode-init ()
+  (custom-set-variables
+    ;; 
+    '(markdown-list-indent-width 2)
+    '(markdown-unordered-list-item-prefix "  - ")
+  )
+)
+
+(add-hook 'markdown-mode-hook 'tahti-markdown-mode-init)
+(provide 'tahti-markdown-mode)
