65804d8533c37801ef121f1d88e6055da840ace1
Piotr Kordy <pkordy@googlemail.com>
2016-10-23 21:14:46 +0200

    added some babel stuff

--- emacs/tahti/lisp/tahti-org.el
+++ emacs/tahti/lisp/tahti-org.el
@@ -1,3 +1,11 @@
 (push 'org-mode el-get-packages)
+(setq org-src-fontify-natively t) ;syntax highlight in code blocks in org mode
+(setq org-confirm-babel-evaluate nil) ; do not ask to evaluate
+(defun tahti-after-org-mode ()
+  (org-babel-do-load-languages 
+   'org-babel-load-languages '((shell . t)
+                               (python . t)))
+)
+
 (provide 'tahti-org)
 
