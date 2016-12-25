246469f4f143f464fa9e1a78538c3514d821a5ad
Piotr Kordy <pkordy@googlemail.com>
2016-12-25 09:13:17 +0100

    Added hosts

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -23,6 +23,13 @@ rot13() { echo $1 | tr "A-Za-z" "N-ZA-Mn-za-m" }
 rot47() { echo $1 | tr "\!-~" "P-~\!-O" }
 latrus() { echo $1 | tr "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM" "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ" }
 ruslat() { echo $1 | tr "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ" "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM" }
-up(){sudo aptitude update && sudo apt-file update && sudo  DEBIAN_FRONTEND=noninteractive aptitude -y upgrade}
+up(){
+  sudo aptitude update && 
+  sudo apt-file update && 
+  sudo  DEBIAN_FRONTEND=noninteractive aptitude -y upgrade &&
+  sudo wget http://winhelp2002.mvps.org/hosts.txt -O /etc/hosts &&
+  sudo cat ~/.etchosts >> /etc/hosts
+
+}
 urlencode() { python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" $1 }
 urldecode() { python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])" $1 }
