f52a17374c89fe89399a6b4514fef38a3b58ab6d
Piotr Kordy <pkordy@googlemail.com>
2018-12-18 19:45:55 +0100

    added comment

--- bin/src/install.sh
+++ bin/src/install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#By default, only root can change the brightness by this method. To allow users in the video group to change the brightness, a udev rule such as the following can be used:
+
+#/etc/udev/rules.d/backlight.rules
+
+#ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="acpi_video0", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
+#ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="acpi_video0", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
+
 TMPDIR=$(mktemp -d -t gitXXXX)
 #Install brightness
 cd $TMPDIR
